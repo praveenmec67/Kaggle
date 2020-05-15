175c6ce0f2e5e2373349547d44dfe520f030038a
Praveen Thenraj <59313028+praveenmec67@users.noreply.github.com>
2020-05-15 01:03:15 -0400

    today upc ratio

--- Transformation_Base_Model_V0.1.py
+++ Transformation_Base_Model_V0.1.py
@@ -20,9 +20,6 @@ def process_train_cal():
 
     df=pd.read_csv('sales_train_validation.csv')
 
-    print(df.dept_id.unique())
-    print(df.cat_id.unique())
-
     cal=pd.read_csv('calendar.csv')
 
     cal_d = cal['d'].tolist()
@@ -38,8 +35,11 @@ def process_train_cal():
     df=pd.concat([df1,df2],axis=1).drop('id',axis=1)
 
     print('entering melt')
+
     df=pd.melt(id_vars=['item_id','dept_id','cat_id','store_id','state_id'],value_vars=df.iloc[:,5:],var_name='Date',value_name='Sales',frame=df)
+
     print('exiting melt')
+
 
 #Creating features for the model:
 
@@ -51,21 +51,37 @@ def train_features():
 
     fun1=lambda x:1 if x==True else(0)
 
+    start_time=time.time()
     df['dayofyear']=pd.DatetimeIndex(df['Date']).dayofyear
+    print('dayofyear time: {:.2f}s'.format(time.time()-start_time))
 
+    start_time = time.time()
     df['weekofyear']=pd.DatetimeIndex(df['Date']).weekofyear
+    print('weekofyear time: {:.2f}s'.format(time.time() - start_time))
 
+    start_time = time.time()
     df['monthstart']=pd.DatetimeIndex(df['Date']).is_month_start
+    print('monthstart time: {:.2f}s'.format(time.time() - start_time))
 
+    start_time = time.time()
     df['monthstart']=df['monthstart'].apply(fun1)
+    print('monthstart apply time: {:.2f}s'.format(time.time() - start_time))
 
+    start_time = time.time()
     df['quarterstart']=pd.DatetimeIndex(df['Date']).is_quarter_start
+    print('quarterstart time: {:.2f}s'.format(time.time() - start_time))
 
+    start_time = time.time()
     df['quarterstart']=df['quarterstart'].apply(fun1)
+    print('quarterstart apply time: {:.2f}s'.format(time.time() - start_time))
 
+    start_time = time.time()
     df['yearstart']=pd.DatetimeIndex(df['Date']).is_year_start
+    print('yearstart time: {:.2f}s'.format(time.time() - start_time))
 
+    start_time = time.time()
     df['yearstart']=df['yearstart'].apply(fun1)
+    print('yearstart apply time: {:.2f}s'.format(time.time() - start_time))
 
     df1=df['Sales']
 
@@ -73,9 +89,10 @@ def train_features():
 
     df_final=pd.concat([df2,df1],axis=1)
 
-    print(df_final.head())
+    df_final.to_pickle('df_final')
 
     print('exiting train features')
+
 
 #Aggregating at the category level:
 
@@ -83,7 +100,9 @@ def cat_agg():
 
     print('entering cat_agg')
 
-    global df_foods,df_household,df_hobbies
+    global df_foods,df_household,df_hobbies,df_final
+
+    df_final = pd.read_pickle('df_final')
 
     df_cat=df_final.groupby(['cat_id','store_id','Date','dayofyear','weekofyear','monthstart','quarterstart','yearstart'])['Sales'].sum()
 
@@ -95,9 +114,10 @@ def cat_agg():
 
     df_hobbies = df_cat[df_cat['cat_id'] == 'HOBBIES']
 
-    print(df_foods.head())
+    print(df_hobbies.head(3))
 
     print('exiting cat_agg')
+
 
 #Aggregating at the department level:
 
@@ -105,11 +125,13 @@ def dept_agg():
 
     print('entering dept_agg')
 
-    global df_hobbies_1,df_hobbies_2,df_household_1,df_household_2,df_foods_1,df_foods_2,df_foods_3,df_foods_4
+    global df_dept,df_hobbies_1,df_hobbies_2,df_household_1,df_household_2,df_foods_1,df_foods_2,df_foods_3,df_foods_4
 
     df_dept=df_final.groupby(['dept_id','store_id','Date','dayofyear','weekofyear','monthstart','quarterstart','yearstart'])['Sales'].sum()
 
     df_dept=df_dept.reset_index()
+
+    df_dept=df_dept.rename(columns={'Sales':'Dept Sales'})
 
     df_hobbies_1=df_dept[df_dept['dept_id']=='HOBBIES_1']
 
@@ -127,18 +149,37 @@ def dept_agg():
 
     df_foods_3 = df_dept[df_dept['dept_id'] == 'FOODS_3']
 
-
-    print(df_foods_1.head())
-    print(df_foods_2.head())
-    print(df_foods_3.head())
-
+    print(df_hobbies_1.head(3))
+    print(df_hobbies_2.head(3))
 
     print('exiting dept_agg')
+
+#Calculating the upc sales ratio per store:
+
+def upc_prob():
+
+  #  df_upc_prob=df_dept.groupby(['dept_id','store_id','Date'])['Sales'].sum()
+  #  df_upc_prob=df_upc_prob.reset_index()
+  #  df_upc_prob=df_upc_prob.rename(columns={'Sales':'Dept Sales'})
+  #  df_upc_prob=df_upc_prob.ffill()
+  #  df_upc_prob = df_dept.rename(columns={'Sales': 'Dept Sales'})
+
+    df_trial=df_final[['item_id','dept_id','store_id','Date','Sales']]
+    df_dept_ratio=df_dept[['dept_id','store_id','Date','Dept Sales']]
+
+  # df_merge=df_trial.merge(df_upc_prob,how='inner',on=['dept_id','store_id','Date'])
+    df_merge=df_trial.merge(df_dept,how='inner',on=['dept_id','store_id','Date'])
+    print(df_merge.head(10))
+    df_merge['Ratio'] = df_merge['Sales'] / df_merge['Dept Sales']
+
+
+    print(df_merge.head(10))
 
 
 #Calling the functions:
 
-process_train_cal()
-train_features()
+#process_train_cal()
+#train_features()
 cat_agg()
 dept_agg()
+upc_prob()
