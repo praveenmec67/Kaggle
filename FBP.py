e7d2564bc7858ad7f7666c9f045231a376b23969
Praveen Thenraj <59313028+praveenmec67@users.noreply.github.com>
2020-05-19 01:05:36 -0400

    today_with_add_regressor

--- FBP.py
+++ FBP.py
@@ -15,28 +15,45 @@ pd.set_option('display.max_columns',None)
 os.chdir('/Users/praveenthenraj/Desktop/PycharmProjects/Trial/Kaggle/Competitions')
 
 
-df_final = pd.read_pickle('df_final')
-df_cat=df_final.groupby(['cat_id','store_id','Date','dayofyear','weekofyear','monthstart','quarterstart','yearstart'])['Sales'].sum()
-df_cat=df_cat.reset_index()
-df_foods=df_cat[df_cat['cat_id']=='FOODS']
+#df_final = pd.read_pickle('df_final')
+#df_cat=df_final.groupby(['cat_id','store_id','Date','dayofyear','weekofyear','monthstart','quarterstart','yearstart'])['Sales'].sum()
+#df_cat=df_cat.reset_index()
+#df_foods=df_cat[df_cat['cat_id']=='FOODS']
 
 
-df_foods=df_foods[['Date','Sales']]
-df_foods.columns=['ds','y']
-df_foods_train=df_foods.iloc[0:1000,:]
-df_foods_test=df_foods.iloc[1000:1913,:]
+df_foods=pd.read_csv('df_foods')
 
+df_foods=df_foods[['store_id','Date','Sales']]
+df_foods.columns=['store_id','ds','y']
+stores={'CA_1':1,'CA_2':2,'CA_3':3,'CA_4':4,'TX_1':5,'TX_2':6,'TX_3':7,'WI_1':8,'WI_2':9,'WI_3':10}
+df_foods['store_id']=df_foods['store_id'].map(stores)
+print(df_foods.tail())
+
+
+print('entering model')
 fb = Prophet(interval_width=0.95,yearly_seasonality=True,weekly_seasonality=True)
 fb.add_country_holidays(country_name='US')
-fb.fit(df_foods_train)
-future = fb.make_future_dataframe(freq='D', periods=913,include_history=False)
+fb.add_regressor('store_id')
+fb.fit(df_foods)
+
+
+future = fb.make_future_dataframe(freq='D', periods=28,include_history=False)
+print(future.tail())
+
+a=pd.Series([1,2,3,4,5,6,7,8,9,10])
+stores1=pd.DataFrame({'store_id':a.repeat(28)}).reset_index()
+print(stores1.head())
+
+
+future['store_id']=stores1['store_id']
+print(future.tail())
 predict = fb.predict(future)
+print(predict['yhat'])
 
 
-res=mean_absolute_error(df_foods_test['y'],predict['yhat'])
-print(res)
-
-fig=fb.plot(predict)
-plt.show()
-fig1=fb.plot_components(predict)
-plt.show()
+#res=mean_absolute_error(df_foods_test['y'],predict['yhat'])
+#print(res)
+#fig=fb.plot(predict)
+#plt.show()
+#fig1=fb.plot_components(predict)
+#plt.show()
